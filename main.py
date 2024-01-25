4003e24bfad2bb36887c71c3f48a40e373ee6ed2
TahaThinks <tahaa4587@gmail.com>
2024-01-25 10:54:09 +0400

    Import PrettyTable

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-import turtle
-from turtle import Turtle, Screen
+# import turtle
+# from turtle import Turtle, Screen
 
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+import prettytable
