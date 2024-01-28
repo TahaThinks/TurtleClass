d6840381dbce73e71c20050626c3e41d6c1048ac
TahaThinks <tahaa4587@gmail.com>
2024-01-28 14:37:43 +0400

    left align the table

--- main.py
+++ main.py
@@ -17,4 +17,5 @@ table = PrettyTable()
 print(table)
 table.add_column("Pokemon",["Pikachu","Squirtle","Charmander"])
 table.add_column("Type",["Electric","Water","Fire"])
+table.align = "l"
 print(table)
