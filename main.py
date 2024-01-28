cc317989b4647e508bb5643fe3324ce259916bdd
TahaThinks <tahaa4587@gmail.com>
2024-01-28 14:30:50 +0400

    Import PrettyTable

--- main.py
+++ main.py
@@ -14,3 +14,7 @@
 
 from prettytable import PrettyTable
 table = PrettyTable()
+print(table)
+table.add_column("Pokemon",["Pikachu","Squirtle","Charmander"])
+table.add_column("Type",["Electric","Water","Fire"])
+print(table)
