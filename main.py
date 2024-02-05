583627c32f43f70cc8085fe03c08c6c2c57b7d7b
TahaThinks <tahaa4587@gmail.com>
2024-02-05 11:25:30 +0400

    Refine Code and Generate Random Colors

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import turtle
 from turtle import Turtle, Screen
-from random import randrange,choice
+from random import randrange,choice,randint
+from typing import Tuple
 
+turtle.colormode(255)
 
 tim = Turtle()
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 directions = [0, 90, 180, 270]
 tim.pensize(5)
 tim.speed('fastest')
 
+def random_color():
+    r = randint(0,255)
+    g = randint(0,255)
+    b = randint(0,255)
+    random_color = (r, g, b)
+    return random_color
+
+
+
 for _ in range(500):
-    tim.pencolor(choice(colours))
+    tim.pencolor(random_color())
     tim.forward(30)
     tim.setheading(choice(directions))
 
