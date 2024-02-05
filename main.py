376eb2695891131b9ad123d651e82ab2e74db4e9
TahaThinks <tahaa4587@gmail.com>
2024-02-05 10:47:20 +0400

    Draw Several Shapes, with different colors

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
+import turtle
 from turtle import Turtle, Screen
+from random import randint
+turtle.colormode(255)
 
 tim = Turtle()
+sides = list(range(3, 20))
 
 
-def dashed_color():
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+def line_color():
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+    print(r, g, b)
+    tim.pencolor((r, g, b))
+
+def draw_shape(side, angle):
+    line_color()
+    for _ in range(side):
+        tim.forward(100)
+        tim.right(angle)
 
 
-for _ in range(15):
-    dashed_color()
+for shape_side in sides:
+    draw_shape(shape_side, 360 / shape_side)
+
+
 
 screen = Screen()
 screen.exitonclick()
