da13e8723b8b1296992f1f02b5b1333c3e9521f0
TahaThinks <tahaa4587@gmail.com>
2024-02-05 11:27:08 +0400

    Refine Code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import turtle
 from turtle import Turtle, Screen
-from random import randrange,choice,randint
+from random import randrange, choice, randint
 from typing import Tuple
 
 turtle.colormode(255)
@@ -10,13 +10,13 @@ directions = [0, 90, 180, 270]
 tim.pensize(5)
 tim.speed('fastest')
 
+
 def random_color():
-    r = randint(0,255)
-    g = randint(0,255)
-    b = randint(0,255)
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
     random_color = (r, g, b)
     return random_color
-
 
 
 for _ in range(500):
